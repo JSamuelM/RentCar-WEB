535d405960134893ea7fe2d50611f4650dae134f
bernaluis <bernal.lrm@hotmail.com>
2018-10-16 23:45:30 -0600

    Arreglo

--- web/js/brand.js
+++ web/js/brand.js
@@ -129,7 +129,7 @@ function update(){
     var nomb=$('#nameupd').val();
 
     $.ajax({
-        url : 'http://ec2-52-14-245-189.us-east-2.compute.amazonaws.com:8080/JerseyHibernateRent/webapi/update',
+        url : 'http://ec2-52-14-245-189.us-east-2.compute.amazonaws.com:8080/JerseyHibernateRent/webapi/Marca/update',
         headers: { 
             
             'Content-Type': 'application/json' 

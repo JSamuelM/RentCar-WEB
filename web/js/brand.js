d9c572036cfa8b1a61d6609e83653a2acd5f91f0
bernaluis <bernal.lrm@hotmail.com>
2018-10-16 23:47:37 -0600

    Arreglo

--- web/js/brand.js
+++ web/js/brand.js
@@ -176,7 +176,7 @@ function remove(){
     var codi=$('#deleCodi').val();
     var nomb=$('#nameupd').val();
     $.ajax({
-        url : 'http://ec2-52-14-245-189.us-east-2.compute.amazonaws.com:8080/JerseyHibernateRent/webapi/delete',
+        url : 'http://ec2-52-14-245-189.us-east-2.compute.amazonaws.com:8080/JerseyHibernateRent/webapi/Marca/delete',
         headers: { 
             
             'Content-Type': 'application/json' 
